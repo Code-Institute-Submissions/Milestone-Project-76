1aedfe06c2ff9d54b4e7e099447b01a063c7daee
Niall <n_sugrue@hotmail.com>
2020-06-10 14:39:35 +0000

    Move over from C9 added google maps with markers

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: {
+            lat: 53.270974,
+            lng: -9.056724
+        }
+    });
+}
